0bf6b87ad2f65992ea5c822c1c5cb977322bd44c
Steve Wells <yxes123@gmail.com>
2015-08-11 13:51:20 -0700

    Original Site

---
+++ Francais_files/Francais.js
@@ -0,0 +1,11 @@
+// Created by iWeb 3.0.1 local-build-20090913
+
+setTransparentGifURL('Media/transparent.gif');function applyEffects()
+{var registry=IWCreateEffectRegistry();registry.registerEffects({stroke_0:new IWPhotoFrame([IWCreateImage('Francais_files/Creme_frame2_01.png'),IWCreateImage('Francais_files/Creme_frame2_02.png'),IWCreateImage('Francais_files/Creme_frame2_04.png'),IWCreateImage('Francais_files/Creme_frame2_08.png'),IWCreateImage('Francais_files/Creme_frame2_09.png'),IWCreateImage('Francais_files/Creme_frame2_14.png'),IWCreateImage('Francais_files/Creme_frame2_13.png'),IWCreateImage('Francais_files/Creme_frame2_05.png')],null,0,1.000000,3.000000,3.000000,3.000000,3.000000,23.000000,23.000000,23.000000,23.000000,20.000000,19.000000,20.000000,19.000000,null,null,null,0.400000),stroke_1:new IWPhotoFrame([IWCreateImage('Francais_files/Creme_frame2_01_1.png'),IWCreateImage('Francais_files/Creme_frame2_02_1.png'),IWCreateImage('Francais_files/Creme_frame2_04_1.png'),IWCreateImage('Francais_files/Creme_frame2_08_1.png'),IWCreateImage('Francais_files/Creme_frame2_09_1.png'),IWCreateImage('Francais_files/Creme_frame2_14_1.png'),IWCreateImage('Francais_files/Creme_frame2_13_1.png'),IWCreateImage('Francais_files/Creme_frame2_05_1.png')],null,0,1.000000,3.000000,3.000000,3.000000,3.000000,23.000000,23.000000,23.000000,23.000000,20.000000,19.000000,20.000000,19.000000,null,null,null,0.400000)});registry.applyEffects();}
+function hostedOnDM()
+{return false;}
+function onPageLoad()
+{loadMozillaCSS('Francais_files/FrancaisMoz.css')
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');fixAllIEPNGs('Media/transparent.gif');Widget.onload();fixupIECSS3Opacity('id2');applyEffects()}
+function onPageUnload()
+{Widget.onunload();}
